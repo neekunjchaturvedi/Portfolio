f5c9627128a97231d452f357ec29324b4c26d84d
neekunjchaturvedi <neekunjchaturvedi3@gmail.com>
2024-11-02 11:38:07 +0530

    Deploy

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const phrases = ["Web Developer/>", "UI/UX Designer/>", "Programmer/>"];
 let phraseIndex = 0;
 let charIndex = 0;
-const typingSpeed = 75; // milliseconds per character
-const erasingSpeed = 75; // milliseconds per character
-const delayBetweenPhrases = 1500; // milliseconds before switching phrases
+const typingSpeed = 75;
+const erasingSpeed = 75;
+const delayBetweenPhrases = 1500;
 const typingTextElement = document.getElementById("typing-text");
 
 function typePhrase() {
   if (charIndex < phrases[phraseIndex].length) {
-    // Typing effect
     typingTextElement.innerHTML += phrases[phraseIndex].charAt(charIndex);
     charIndex++;
     setTimeout(typePhrase, typingSpeed);
   } else {
-    // Erasing effect after a delay
     setTimeout(erasePhrase, delayBetweenPhrases);
   }
 }
@@ -27,49 +25,14 @@ function erasePhrase() {
     charIndex--;
     setTimeout(erasePhrase, erasingSpeed);
   } else {
-    // Move to the next phrase after erasing
     phraseIndex = (phraseIndex + 1) % phrases.length;
     setTimeout(typePhrase, typingSpeed);
   }
 }
 
-// Start the typing effect
 document.addEventListener("DOMContentLoaded", function () {
   typePhrase();
 });
-
-function sendEmail() {
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const message = document.getElementById("message").value;
-
-  // Check if all fields are filled
-  if (name && email && message) {
-    Email.send({
-      SecureToken: "ecf6d2c5-1641-4321-b930-76a6dac02b99",
-      To: "neekunjchaturvedi3@gmail.com",
-      From: email,
-      Subject: `New message from ${name}`,
-      Body: `
-          <b>Sender:</b> ${name} (${email})<br><br>
-          <b>Message:</b><br>${message}
-        `,
-    })
-      .then((response) => {
-        if (response === "OK") {
-          alert("Message sent successfully");
-        } else {
-          alert("Failed to send message: " + response);
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        alert("An error occurred while sending the message.");
-      });
-  } else {
-    alert("Please fill in all fields");
-  }
-}
 
 function toggleNav() {
   const navLinks = document.getElementById("navLinks");
@@ -79,7 +42,7 @@ function closeNav() {
   const navLinks = document.getElementById("navLinks");
   navLinks.classList.add("hidden");
 }
-// Close navbar when clicking outside
+
 document.addEventListener("click", function (event) {
   const navLinks = document.getElementById("navLinks");
   const navToggle = document.getElementById("navToggle");
